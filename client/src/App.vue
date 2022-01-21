70f77f1ccf1e68240d92d09ca0031db356f6b4d9
OnSebii <lang.sebastian.006@gmail.com>
2022-01-21 14:05:32 +0100

    update

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <v-app>
+    <v-app-bar app absolute color="indigo" dark>
+      <v-app-bar-nav-icon>
+        <v-img max-width="50" alt="logo" src="@/assets/bird.png"></v-img>
+      </v-app-bar-nav-icon>
+
+      <v-app-bar-title>Bird Watch</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn color="indigo" class="mx-1" active-class="active" to="/"> Home </v-btn>
+      <v-btn color="indigo" class="mx-1" active-class="active" to="/gallery"> Gallery </v-btn>
+      <v-btn color="indigo" class="mx-1" active-class="active" to="/about"> About </v-btn>
+    </v-app-bar>
     <v-main>
       <v-container> <router-view /></v-container>
     </v-main>
@@ -15,3 +28,5 @@ export default {
   }),
 };
 </script>
+
+<style scoped></style>
